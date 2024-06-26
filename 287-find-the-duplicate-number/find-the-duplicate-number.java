79279e8d51b396693650a117c89a857843c3f07d
Priyansh Chauhan <138983508+PriyanshCh@users.noreply.github.com>
2024-06-27 03:49:34 +0530

    Time: 6 ms (50.52%) | Memory: 58.2 MB (47.43%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,31 +1,23 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-    int i=0;
-    while(i<nums.length){
-        if(nums[i]!=i+1){
-            int correct=nums[i]-1;
-            if(correct<nums.length && nums[i]!=nums[correct]){
-                swap(nums,i,correct);
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]!=i+1){
+                int correct=nums[i]-1;
+                if(correct<nums.length && nums[i]!=nums[correct]){
+                    swap(nums,i,correct);
+                }else{
+                    return nums[i];
+                }
             }else{
-                return nums[i];
+                i++;
             }
-        }else{
-            i++;
         }
-    }return -1;
-    
-    // HashSet<Integer> seen = new HashSet<>();
-    // for (int num : nums) {
-    //     if (seen.contains(num)) {
-    //         return num;
-    //     }
-    //     seen.add(num);
-    // }
-    // return -1;  // Just to satisfy the compiler, this should never be reached
-}
-    void swap(int[] nums,int first, int second){
-        int temp=nums[first];
-        nums[first]=nums[second];
-        nums[second]=temp;
+        return -1;
+    }
+    public void swap(int[] nums,int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
