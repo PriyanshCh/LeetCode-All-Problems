3fc9c5ca23bef628b662ffc6b79430293b085003
Priyansh Chauhan <138983508+PriyanshCh@users.noreply.github.com>
2024-07-11 13:39:20 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (37.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // int m = matrix.length;
-        // int n = matrix[0].length;
-        // int left = 0, right = m * n - 1;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
 
-        // while (left <= right) {
-        //     int mid = left + (right - left) / 2;
-        //     int mid_val = matrix[mid / n][mid % n];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = matrix[mid / n][mid % n];
 
-        //     if (mid_val == target)
-        //         return true;
-        //     else if (mid_val < target)
-        //         left = mid + 1;
-        //     else
-        //         right = mid - 1;
-        // }
-        // return false;
-        int n = matrix.length, m = matrix[0].length;
-
-        // traverse the matrix:
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j]== target)
-                    return true;
-            }
+            if (mid_val == target)
+                return true;
+            else if (mid_val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
         return false;
+        // int n = matrix.length, m = matrix[0].length;
+
+        // // traverse the matrix:
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         if (matrix[i][j]== target)
+        //             return true;
+        //     }
+        // }
+        // return false;
     }
 
 }
