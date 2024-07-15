25e5aaeb10603090c63603330ce843a1fb1c890f
Priyansh Chauhan <138983508+PriyanshCh@users.noreply.github.com>
2024-07-15 18:37:07 +0530

    Time: 2 ms (97.89%) | Memory: 44.9 MB (35.12%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -10,6 +10,15 @@ class Solution {
         //     }
         // }
         // return new int[]{};
+        // Map<Integer,Integer>map=new HashMap<>();
+        // for(int i=0; i<nums.length; i++){
+        //     int compliment=target-nums[i];
+        //     if(map.containsKey(compliment)){
+        //         return new int[]{map.get(compliment),i};
+        //     }
+        //     map.put(nums[i],i);
+        // }
+        // return new int[] {};
         Map<Integer,Integer>map=new HashMap<>();
         for(int i=0; i<nums.length; i++){
             int compliment=target-nums[i];
