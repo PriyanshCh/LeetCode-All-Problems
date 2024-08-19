3c5463a1fe69f39433d83a7bb7d3db1c8dee5e63
Priyansh Chauhan <138983508+PriyanshCh@users.noreply.github.com>
2024-08-19 22:18:31 +0530

    Time: 28 ms (79.30%) | Memory: 51.5 MB (57.33%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,13 +4,13 @@ class Solution {
         Arrays.sort(nums);
 
         for(int i=0; i<nums.length; i++){
-            //remove duplicates
             if(i!=0 && nums[i]==nums[i-1])continue;
 
             int j=i+1;
             int k=nums.length-1;
+
             while(j<k){
-                int sum=nums[i] + nums[j] + nums[k];
+                int sum=nums[i]+nums[j]+nums[k];
 
                 if(sum<0){
                     j++;
@@ -21,13 +21,10 @@ class Solution {
                     ans.add(temp);
                     j++;
                     k--;
-                    //skip the duplicates
                     while(j<k && nums[j]==nums[j-1])j++;
                     while(j<k && nums[k]==nums[k+1])k--;
-
                 }
             }
-        }
-        return ans;
+        }return ans;
     }
 }
