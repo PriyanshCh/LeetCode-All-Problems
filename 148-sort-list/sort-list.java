020cec7cfda11e5e23e495b6a343e0061039a26e
Priyansh Chauhan <138983508+PriyanshCh@users.noreply.github.com>
2025-10-31 23:05:05 +0530

    Time: 9 ms (95.81%) | Memory: 59 MB (5.70%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,62 +1,31 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null || head.next==null){
-            return head;
+        if (head == null || head.next == null) return head;
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ListNode middle=findMiddle(head);
-
-        ListNode right=middle.next;
-        middle.next=null;
-        ListNode left=head;
-
-        left=sortList(left);
-        right=sortList(right);
-
-        return mergeList(left,right);
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        return merge(l1, l2);
     }
-    static ListNode mergeList(ListNode list1, ListNode list2){
-        ListNode dummyNode= new ListNode();
-        ListNode tail=dummyNode;
 
-        while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                tail.next=list1;
-                list1=list1.next;
-            }else{
-                tail.next=list2;
-                list2=list2.next;
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
             }
-            tail=tail.next;
+            curr = curr.next;
         }
-        if(list1!=null){
-            tail.next=list1;
-        }else{
-            tail.next=list2;
-        }
-        return dummyNode.next;
+        curr.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
     }
-    static ListNode findMiddle(ListNode head){
-        if(head==null || head.next==null){
-            return head;
-        }
-        ListNode slow=head;
-        ListNode fast=head.next;
-
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-    
 }
