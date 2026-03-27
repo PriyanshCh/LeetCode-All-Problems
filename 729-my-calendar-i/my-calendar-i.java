d567ceb97d6d383a3b07868fca427b5edc69388d
Priyansh Chauhan <138983508+PriyanshCh@users.noreply.github.com>
2026-03-27 23:31:03 +0530

    Time: 24 ms (67.03%) | Memory: 47.4 MB (39.13%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class MyCalendar {
+    TreeMap<Integer,Integer> map;
+    public MyCalendar() {
+        map=new TreeMap<>();
+    }
+    public boolean book(int start,int end) {
+        Integer prev=map.floorKey(start);
+        if(prev!=null&&map.get(prev)>start) return false;
+        Integer next=map.ceilingKey(start);
+        if(next!=null&&next<end) return false;
+        map.put(start,end);
+        return true;
+    }
+}
